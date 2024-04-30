ae11cf23b1eb3ec1097042d54e01a72d4a08ad69
kotecbr@outlook.com <kotecbr@outlook.com>
2024-04-30 13:07:20 -0300

    Deploy do Blog Pessoal

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,26 +7,26 @@ import { AuthModule } from './auth/auth.module';
 import { Usuario } from './usuario/entities/usuario.entity';
 import { UsuarioModule } from './usuario/usuario.module';
 import { Postagem } from './postagem/entities/postagem.entity';
+import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
+import { ProdService } from './data/services/prod.service';
 
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'db_blogpessoal',
-      entities: [Postagem, Tema, Usuario],
-      synchronize: true,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      useClass: ProdService,
+      /*useClass: DevService 🡢 O Nest executará a aplicação com a configuração do Banco de dados local (MySQL)
+       useClass: ProdService 🡢 O Nest executará a aplicação com a configuração do Banco de dados na nuvem (Render)*/
+      imports: [ConfigModule],
     }),
     PostagemModule,
     TemaModule,
     AuthModule,
     UsuarioModule
   ],
-  controllers: [],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule {}
